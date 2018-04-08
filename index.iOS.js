63c7896d4451ee02efccf4eb2e2bfcdca5a19925
AddIce <addice.ch@gmail.com>
2018-04-09 00:17:12 +0800

    Feat: add index.iOS.js for iOS luanch entrance

---
+++ index.iOS.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import AppiOS from './ios/src/app_ios';
+
+AppRegistry.registerComponent('redkeep', () => AppiOS);
--- ios/redkeep/AppDelegate.m
+++ ios/redkeep/AppDelegate.m
@@ -18,7 +18,7 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.iOS" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"redkeep"
